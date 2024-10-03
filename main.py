63b92505a55141609974920392bf2a81fbf2de11
pablormacia <pablo-macia@live.com.ar>
2024-10-03 15:01:26 -0300

    E10 - Items

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import personaje
 from weapon import Weapon
 import os #Para pode trabajar con archivos y carpetas
 from textos import DamageText
+from items import Item
 
 #Correr en la consola: venv/Scripts/activate
 
@@ -34,31 +35,31 @@ font = pygame.font.Font("assets//fonts//AbaddonBold.ttf")#Uso doble barras para 
 
 #Importar imagenes
 
-corazon_vacio = pygame.image.load(f"assets\images\items\heart-empty.png").convert_alpha()
+corazon_vacio = pygame.image.load(f"assets//images//items//heart-empty.png").convert_alpha()
 corazon_vacio = escalar_img(corazon_vacio, constantes.ESCALA_CORAZON)
-corazon_mitad = pygame.image.load(f"assets\images\items\heart-half.png").convert_alpha()
+corazon_mitad = pygame.image.load(f"assets//images//items//heart-half.png").convert_alpha()
 corazon_mitad = escalar_img(corazon_mitad, constantes.ESCALA_CORAZON)
-corazon_lleno = pygame.image.load(f"assets\images\items\heart-full.png").convert_alpha()
+corazon_lleno = pygame.image.load(f"assets//images//items//heart-full.png").convert_alpha()
 corazon_lleno = escalar_img(corazon_lleno, constantes.ESCALA_CORAZON)
 
 animaciones = []
 for i in range(7):
-    img = pygame.image.load(f"assets\images\characters\players\Player_{i}.png").convert_alpha() #conver_alpha optimiza las imagenes para una mejor velocidad de carga, manteniendo la transparencia
+    img = pygame.image.load(f"assets//images//characters//players//Player_{i}.png").convert_alpha() #conver_alpha optimiza las imagenes para una mejor velocidad de carga, manteniendo la transparencia
     img = escalar_img(img,constantes.ESCALA_PERSONAJE )
     animaciones.append(img)
 
 #enemigos
-directorio_enemigos = 'assets\images\characters\enemies'
+directorio_enemigos = 'assets//images//characters//enemies'
 tipo_enemigos = nombre_carpetas(directorio_enemigos)
 animaciones_enemigos = []
 
 for enemigo in tipo_enemigos:
     lista_temp = []
-    ruta_temp = f"assets\images\characters\enemies\{enemigo}"
+    ruta_temp = f"assets//images//characters//enemies//{enemigo}"
     num_animaciones = contar_elementos(ruta_temp)
     #print(num_animaciones)
     for i in range(num_animaciones):
-        img_enemigo = pygame.image.load(f"{ruta_temp}\{enemigo}-{i+1}.png").convert_alpha()
+        img_enemigo = pygame.image.load(f"{ruta_temp}//{enemigo}-{i+1}.png").convert_alpha()
         img_enemigo = escalar_img(img_enemigo,constantes.ESCALA_ENEMIGOS)
         lista_temp.append(img_enemigo)
     animaciones_enemigos.append(lista_temp)
@@ -66,11 +67,22 @@ for enemigo in tipo_enemigos:
 #print(animaciones_enemigos)
 
 #Imagenes
-imagen_pistola = pygame.image.load(f"assets\images\weapons\Glock18.png").convert_alpha()
+imagen_pistola = pygame.image.load(f"assets//images//weapons//Glock18.png").convert_alpha()
 imagen_pistola = escalar_img(imagen_pistola,constantes.ESCALA_PISTOLA)
 
-imagen_bala = pygame.image.load(f"assets\images\weapons\Bulletuzi.png").convert_alpha()
+imagen_bala = pygame.image.load(f"assets//images//weapons//Bulletuzi.png").convert_alpha()
 imagen_bala = escalar_img(imagen_bala,constantes.ESCALA_BALA)
+
+pocion_roja = pygame.image.load(f"assets//images/items//potion.png").convert_alpha()
+pocion_roja = escalar_img(pocion_roja, 1)
+
+coin_images = []
+ruta_img = "assets//images//items//coin"
+num_coin_images = contar_elementos(ruta_img)
+for i in range(num_coin_images):
+    img = pygame.image.load(f"assets//images//items//coin//coin-{i+1}.png")
+    img = escalar_img(img,1)
+    coin_images.append(img)
 
 def vida_jugador():
     c_mitad_dibujado = False
@@ -104,10 +116,18 @@ pistola = Weapon(imagen_pistola,imagen_bala)
 grupo_damage_text = pygame.sprite.Group() #Es necesario ya que puede mostrarse más de un texto al mismo tiempo
 grupo_balas = pygame.sprite.Group() #Porque pueden haber varias balas en la pantalla
 
+
+
 #temporal y borrar
 """damage_text = DamageText(100,240,"25",font,constantes.ROJO)
 grupo_damage_text.add(damage_text)"""
 
+grupo_items = pygame.sprite.Group()
+coin = Item(350, 25, 0, coin_images)
+potion = Item(380, 55, 1, [pocion_roja])
+
+grupo_items.add(coin)
+grupo_items.add(potion)
 
 
 #Definir las variables de movimiento del jugador
@@ -166,6 +186,10 @@ while run:
     #Actualizar el daño
     grupo_damage_text.update() #Al heredar de la clase sprite ya tiene el método update, a diferencia de personaje por ej que lo tuvimos que crear a mano
 
+    #Actualizar items
+    grupo_items.update() #puedo usar el método porque uso sprites
+
+
     #Dibujar al jugador
     jugador.dibujar(ventana)
 
@@ -183,8 +207,11 @@ while run:
     #Dibujar las vidas
     vida_jugador()
 
-    #Dibujar texto
+    #Dibujar textos
     grupo_damage_text.draw(ventana)
+
+    #Dibujar items
+    grupo_items.draw(ventana)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
